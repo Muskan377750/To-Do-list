13384b01320f7d76eff61fd67b1cc11034ce7075
Muskan377750 <nov6947sk@gmail.com>
2025-10-07 15:56:50 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,18 @@
+# To-Do-List
+
+A to-do list is a written or digital list of tasks you plan to complete. Each item represents a specific action or goal, and the list helps you stay focused and organized.
+
+# Working:-
+
+- Add Tasks: Write down everything you need to do.
+- Review & Update: Add new tasks or remove completed ones regularly.
+
+# Helps in:-
+
+- Reduces mental clutter
+- Improves time management
+- Boosts productivity and motivation
+- Helps prioritize effectively
+
+
+
---
+++ app.js
@@ -0,0 +1,22 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("Button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+  inp.value = "";
+  ul.appendChild(item);
+
+  let del = document.createElement("button");
+  del.innerText = "Delete";
+  del.classList.add = "delete";
+  item.appendChild(del);
+});
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();  
+    }
+});
