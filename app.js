a1b2571935e8c0062bc1da6c52a5c3e94112d56f
Muskan377750 <nov6947sk@gmail.com>
2026-04-17 13:20:00 +0530

    fixed bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let inp = document.querySelector("input");
-let btn = document.querySelector("Button");
+let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 
 btn.addEventListener("click", function () {
